58cb12cc4ea312097fa0b6ea7202c543e0fcf4ff
Sean O'Connor <so@seano.dev>
2023-09-15 16:51:00 -0600

    Add 404 error handler

--- app/__init__.py
+++ app/__init__.py
@@ -6,3 +6,8 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return render_template('index.html')
+
+
+@app.errorhandler(404)
+def errorhandler_404(error):
+    return render_template('404.html', error=error), 404
